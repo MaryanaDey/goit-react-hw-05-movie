9badf6e039290da4dafa87d122ddf1cfea82117c
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2022-02-02 18:14:04 +0200

    add notefound

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('./routes/Home/Home' /* webpackChunkName: "home-view" */));
+const NotFound = lazy(() =>
+  import('./views/NoteFound/NoteFound.jsx' /* webpackChunkName: "not-found-view" */),
+);
+const Movies = lazy(() =>
+  import('./views/Movies/Movies.jsx' /* webpackChunkName: "movies-view" */),
+);
 
 export default function App() {
-  return <>App</>;
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </Suspense>
+  );
 }
